95af0cef692a72e557fc9387f0d2bee29e87377a
karnatsk <158044167+karnatsk@users.noreply.github.com>
2024-04-26 22:12:25 +0300

    Update Copter.h

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -606,7 +606,8 @@ private:
         SMARTRTL_LAND      = 4,
         TERMINATE          = 5,
         AUTO_DO_LAND_START = 6,
-        BRAKE_LAND         = 7
+        BRAKE_LAND         = 7,
+        GUIDED_NOGPS       = 8
     };
 
     enum class FailsafeOption {
@@ -777,6 +778,7 @@ private:
     void set_mode_auto_do_land_start_or_RTL(ModeReason reason);
     void set_mode_brake_or_land_with_pause(ModeReason reason);
     bool should_disarm_on_failsafe();
+    void set_mode_guided_nogps(ModeReason reason);
     void do_failsafe_action(FailsafeAction action, ModeReason reason);
     void announce_failsafe(const char *type, const char *action_undertaken=nullptr);
 
