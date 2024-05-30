9749afb7c54e721377db4bf2678ed99910e1e244
Danylo Kasianenko <thedajes@gmail.com>
2024-05-30 09:31:08 +0300

    Fashion is my profession

--- libraries/AP_OSD/AP_OSD_Screen.cpp
+++ libraries/AP_OSD/AP_OSD_Screen.cpp
@@ -1596,29 +1596,33 @@ void AP_OSD_Screen::draw_horizon(uint8_t x, uint8_t y)
     if (target_bbox.is_valid) {
         const int height = 16;
         const int width = 30;
-        float x_1 = target_bbox.x1 * width + 0.25f;
-        float x_2 = target_bbox.x2 * width + 0.25f;
-        float y_1 = target_bbox.y1 * height + 0.25f;
-        float y_2 = target_bbox.y2 * height + 0.25f;
+        float x_1 = target_bbox.x1 * width;
+        float y_1 = target_bbox.y1 * height;
+        float x_2 = target_bbox.x2 * width;
+        float y_2 = target_bbox.y2 * height;
 
         int x_1_i = floorf(x_1);
-        int x_2_i = ceilf(x_2);
         int y_1_i = floorf(y_1);
-        int y_2_i = ceilf(y_2);
+        int x_2_i = floorf(x_2);
+        int y_2_i = floorf(y_2);
 
         char c_x1 = SYMBOL(SYM_AH_V_START) + (x_1 - x_1_i) * SYMBOL(SYM_AH_V_COUNT);
         char c_x2 = SYMBOL(SYM_AH_V_START) + (x_2 - x_2_i) * SYMBOL(SYM_AH_V_COUNT);
         char c_y1 = SYMBOL(SYM_AH_H_START) + (y_1 - y_1_i) * SYMBOL(SYM_AH_H_COUNT);
         char c_y2 = SYMBOL(SYM_AH_H_START) + (y_2 - y_2_i) * SYMBOL(SYM_AH_H_COUNT);
 
-        for(int j = y_1_i; j < y_2_i; j++) {
+        for(int j = y_1_i + 1; j < y_2_i; j++) {
             backend->write(x_1_i, j, false, "%c", c_x1);
             backend->write(x_2_i, j, false, "%c", c_x2);
         }
-        for(int i = x_1_i; i < x_2_i; i++) {
+        for(int i = x_1_i + 1; i < x_2_i; i++) {
             backend->write(i, y_1_i, false, "%c", c_y1);
             backend->write(i, y_2_i, false, "%c", c_y2);
         }
+        backend->write(x_1_i, y_1_i, false, "%c", c_x1);
+        backend->write(x_1_i, y_2_i, false, "%c", c_x1);
+        backend->write(x_2_i, y_1_i, false, "%c", c_x2);
+        backend->write(x_2_i, y_2_i, false, "%c", c_x2);
     }
 
 }
